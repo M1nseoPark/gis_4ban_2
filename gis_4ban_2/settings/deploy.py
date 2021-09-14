f8d9f2d93e38a2c03e52c3f99a33b22cf6544ba6
MinseoPark <kjms0820@gmail.com>
2021-09-14 11:26:27 +0900

    9/14 4ban class commit 1

---
+++ gis_4ban_2/settings/deploy.py
@@ -0,0 +1,18 @@
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'password1234',
+        'HOST': 'mariadb',
+        'PORT': '3306',
+    }
+}
